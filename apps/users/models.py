f8d06d85acdb2ade732798d4f9dc0185d91b7d3a
Mukhammed-Amin <muhammadaminibaydillayev@gmail.com>
2025-06-21 16:46:19 +0600

    Lesson 3

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,10 +3,17 @@ from django.db import models
 # Create your models here.
 class InfoUser(models.Model):
     name = models.CharField(max_length=255, verbose_name="Имя")
+    email = models.EmailField()
+    image = models.ImageField(upload_to='infouser/', verbose_name="Фотография")
+    work = models.CharField(max_length=255, verbose_name="Работа")
+    description = models.TextField(verbose_name="Описание")
+    twitter = models.URLField(verbose_name="Твиттер")
+    telegram = models.URLField(verbose_name="Телеграм")
+    linkedin = models.URLField(verbose_name="Линкед Ин")
+    github = models.URLField(verbose_name="Гитхаб")
     
-
     def __str__(self):
-        return self.name
+        return f"{self.id} / {self.name} / {self.email} / {self.work}"
     
     class Meta:
         verbose_name = "Информация о пользователе"
